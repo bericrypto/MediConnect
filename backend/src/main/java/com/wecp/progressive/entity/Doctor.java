5af135394b6c48ae760368dd7f37b0d8b7cde1f6
Ubuntu <ubuntu@ip-172-31-47-22.ap-south-1.compute.internal>
2025-09-01 11:24:39 +0000

    day 1 task 1

--- backend/src/main/java/com/wecp/progressive/entity/Doctor.java
+++ backend/src/main/java/com/wecp/progressive/entity/Doctor.java
@@ -1,5 +1,62 @@
 package com.wecp.progressive.entity;
 
-public class Doctor {
+import java.sql.Date;
 
+public class Doctor {
+    private int doctorId;
+    private String fullName;
+    private Date dateOfBirth;
+    private long contactNumber;
+    private String email;
+    private String address;
+
+    public Doctor() {
+    }
+
+    public Doctor(int doctorId, String fullName, Date dateOfBirth, long contactNumber, String email, String address) {
+        this.doctorId = doctorId;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.address = address;
+    }
+    
+    public int getDoctorId() {
+        return doctorId;
+    }
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public long getContactNumber() {
+        return contactNumber;
+    }
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
 }
