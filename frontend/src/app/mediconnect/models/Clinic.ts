1743d18ca4fee3318847bbf7ee97511fba104d43
Ubuntu <ubuntu@ip-172-31-21-252.ap-south-1.compute.internal>
2025-09-17 11:51:11 +0000

    Day 16

--- frontend/src/app/mediconnect/models/Clinic.ts
+++ frontend/src/app/mediconnect/models/Clinic.ts
@@ -1,4 +1,33 @@
-
 export class Clinic {
-  
-}
+    clinicId: number;
+    clinicName: string;
+    location: string;
+    doctorId: number;
+    contactNumber: string;
+    establishedYear: number;
+   
+    constructor(
+      clinicId: number,
+      clinicName: string,
+      location: string,
+      doctorId: number,
+      contactNumber: string,
+      establishedYear: number
+    ) {
+      this.clinicId = clinicId;
+      this.clinicName = clinicName;
+      this.location = location;
+      this.doctorId = doctorId;
+      this.contactNumber = contactNumber;
+      this.establishedYear = establishedYear;
+    }
+   
+    logAttributes(): void {
+      console.log('clinicId:', this.clinicId);
+      console.log('clinicName:', this.clinicName);
+      console.log('location:', this.location);
+      console.log('doctorId:', this.doctorId);
+      console.log('contactNumber:', this.contactNumber);
+      console.log('establishedYear:', this.establishedYear);
+    }
+  }
